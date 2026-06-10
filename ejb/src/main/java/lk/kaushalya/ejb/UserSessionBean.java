323a9e1237c8cae9b6c27780d03fed199cc86cdb
Ishara Sahan Kaushalya <sahankaushalya.info@gmail.com>
2026-06-10 14:24:30 +0530

    Here is a concise commit message summarizing the specific changes in your diffs:
    ```text
    Implement UserService in UserSessionBean as a stateless EJB
    
    - Annotate UserSessionBean with jakarta.ejb.Stateless
    - Make UserSessionBean implement the lk.kaushalya.core.service.UserService interface
    - Add stub implementations for getUserByID, addUser, updateUser, deleteUser, and getAllUsers methods
    ```

--- ejb/src/main/java/lk/kaushalya/ejb/UserSessionBean.java
+++ ejb/src/main/java/lk/kaushalya/ejb/UserSessionBean.java
@@ -1,4 +1,36 @@
 package lk.kaushalya.ejb;
 
-public class UserSessionBean {
+import jakarta.ejb.Stateless;
+import lk.kaushalya.core.dto.UserDTO;
+import lk.kaushalya.core.service.UserService;
+
+import java.util.List;
+
+@Stateless
+public class UserSessionBean implements UserService {
+    @Override
+    public UserDTO getUserByID(Long id) {
+        System.out.println("getUserById");
+        return null;
+    }
+
+    @Override
+    public void addUser(UserDTO dto) {
+        System.out.println("addUser");
+    }
+
+    @Override
+    public void updateUser(UserDTO dto) {
+        System.out.println("updateUser");
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        System.out.println("deleteUser");
+    }
+
+    @Override
+    public List<UserDTO> getAllUsers() {
+        return List.of();
+    }
 }
